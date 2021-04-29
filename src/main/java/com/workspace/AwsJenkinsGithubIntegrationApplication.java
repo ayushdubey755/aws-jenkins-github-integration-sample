b49adc7f6081e8d0d87c89d91733ceb5162e23ea
aydubeyTGS <aydubey@teksystems.com>
2021-04-29 16:23:30 +0530

    first commit

---
+++ src/main/java/com/workspace/AwsJenkinsGithubIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJenkinsGithubIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJenkinsGithubIntegrationApplication.class, args);
+	}
+
+}
